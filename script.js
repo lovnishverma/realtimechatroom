70722dcbad57a434795494da421e6573c9dd8bf0
Glitch (glitch-hello-website) <none>
2025-03-29 17:02:50 +0000

    🤑💐 Checkpoint ./script.js:75282471/2986 ./style.css:75282471/11518 ./index.html:75282471/18778 ./.glitch-assets:75282471/752

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
 /*
-  This is your site JavaScript code - you can add interactivity!
+  Inspired by: "Login Page & Homepage"
+  By: Neo
+  Link: https://dribbble.com/shots/4485321-Login-Page-Homepage
 */
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+let usernameInput = document.querySelector('.username');
+let passwordInput = document.querySelector('.password');
+let showPasswordButton = document.querySelector('.password-button');
+let face = document.querySelector('.face');
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
-    btn.classList.toggle("dipped");
-  };
-}
+passwordInput.addEventListener('focus', event => {
+  document.querySelectorAll('.hand').forEach(hand => {
+    hand.classList.add('hide');
+  });
+  document.querySelector('.tongue').classList.remove('breath');
+});
 
+passwordInput.addEventListener('blur', event => {
+  document.querySelectorAll('.hand').forEach(hand => {
+    hand.classList.remove('hide');
+    hand.classList.remove('peek');
+  });
+  document.querySelector('.tongue').classList.add('breath');
+});
 
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
+usernameInput.addEventListener('focus', event => {
+  let length = Math.min(usernameInput.value.length - 16, 19);
+  document.querySelectorAll('.hand').forEach(hand => {
+    hand.classList.remove('hide');
+    hand.classList.remove('peek');
+  });
 
-// Open file when the link in the preview is clicked
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
-  );
-};
-// Get the file opening button from its class name
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
+  face.style.setProperty('--rotate-head', `${-length}deg`);
+});
+
+usernameInput.addEventListener('blur', event => {
+  face.style.setProperty('--rotate-head', '0deg');
+});
+
+usernameInput.addEventListener('input', _.throttle(event => {
+  let length = Math.min(event.target.value.length - 16, 19);
+
+  face.style.setProperty('--rotate-head', `${-length}deg`);
+}, 100));
+
+showPasswordButton.addEventListener('click', event => {
+  if (passwordInput.type === 'text') {
+    passwordInput.type = 'password';
+    document.querySelectorAll('.hand').forEach(hand => {
+      hand.classList.remove('peek');
+      hand.classList.add('hide');
+    });
+  } else {
+    passwordInput.type = 'text';
+    document.querySelectorAll('.hand').forEach(hand => {
+      hand.classList.remove('hide');
+      hand.classList.add('peek');
+    });
+  }
 });
