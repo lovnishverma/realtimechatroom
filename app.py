a742ce7a6d4172885326ead76827d9c7ed8141a1
Lovnish Verma <princelv84@gmail.com>
2025-07-13 19:04:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import re
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'superduper_secret_key'
-socketio = SocketIO(app, async_mode='gevent')
+socketio = SocketIO(app, async_mode='gevent', cors_allowed_origins='*')
+
 
 # MongoDB setup
 MONGO_URI = "mongodb+srv://test:test@cluster0.sxci1.mongodb.net/chatDB?retryWrites=true&w=majority"
